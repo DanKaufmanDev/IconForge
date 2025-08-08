493df4ececfcbfba8dd0941e372c99a2aac38fd1
Dan Kaufman <daniel.kaufman_1@yahoo.ca>
2025-08-08 13:46:52 -0400

    updated CDN

--- iconforge-jit.js
+++ iconforge-jit.js
@@ -1,5 +1,5 @@
 (() => {
-  const CDN = 'https://cdn.jsdelivr.net/gh/DanKaufmanDev/IconForge@latest';
+  const CDN = 'https://cdn.jsdelivr.net/gh/DanKaufmanDev/IconForge@jit-dev';
   const FONT_NAME = 'IconForge';
   const FONT_URL = `${CDN}/iconforge.woff2`;
   const META_ICONS_URL = `${CDN}/meta/iconforge-icons.json`;
