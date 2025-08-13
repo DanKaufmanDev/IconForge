7aa7b60541180213ad06b6760825ec9c8475e5b8
Dan Kaufman <daniel.kaufman_1@yahoo.ca>
2025-08-12 20:44:46 -0400

    updated

--- dist/iconforge.js
+++ dist/iconforge.js
@@ -1,6 +1,6 @@
 (() => {
   const VERSION = '1.1.6';
-  const CDN = 'https://cdn.jsdelivr.net/gh/DanKaufmanDev/IconForge@75dd8b5/dist';
+  const CDN = 'https://cdn.jsdelivr.net/gh/DanKaufmanDev/IconForge@01afbbc/dist';
   const FONT_NAME = 'IconForge';
   const FONT_URL = `${CDN}/iconforge.woff2`;
   const META_ICONS_URL = `${CDN}/meta/iconforge-icons.json`;
